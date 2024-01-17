7a5f98f60c5de5571f9e965fc733ec09717077d0
Andy Terry <andyterrydesign@gmail.com>
2024-01-17 13:56:15 +0000

    Second Update

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./components/**/*.{vue,js,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./app.vue",
+    // You can also include any other file where you use Tailwind classes
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
